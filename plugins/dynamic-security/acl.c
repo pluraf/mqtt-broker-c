6ae173a8011c8d10d5b96deafe0a16519895d9aa
Konstantin Tyurin <konstantin@pluraf.com>
2024-09-06 14:14:59 +0200

    Fix of IS-149

--- plugins/dynamic-security/acl.c
+++ plugins/dynamic-security/acl.c
@@ -1,20 +1,55 @@
-/*
+/* SPDX-License-Identifier: BSD-3-Clause */
+
+/******************************************************************************
 Copyright (c) 2020 Roger Light <roger@atchoo.org>
 
 All rights reserved. This program and the accompanying materials
-are made available under the terms of the Eclipse Public License 2.0
-and Eclipse Distribution License v1.0 which accompany this distribution.
+are made available under the terms of Eclipse Distribution License v1.0
+which accompany this distribution.
 
-The Eclipse Public License is available at
-   https://www.eclipse.org/legal/epl-2.0/
-and the Eclipse Distribution License is available at
-  http://www.eclipse.org/org/documents/edl-v10.php.
-
-SPDX-License-Identifier: EPL-2.0 OR BSD-3-Clause
+The Eclipse Distribution License is available at
+  http://www.eclipse.org/org/documents/edl-v10.php
 
 Contributors:
    Roger Light - initial implementation and documentation.
-*/
+
+******************************************************************************/
+
+/******************************************************************************
+Copyright (c) 2024 Pluraf Embedded AB <code@pluraf.com>
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors
+may be used to endorse or promote products derived from this software without
+specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS”
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS
+BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+===============================================================================
+
+Contributors:
+   Konstantin Tyurin <konstantin@pluraf.com>
+
+******************************************************************************/
+
 
 #include "config.h"
 
@@ -174,30 +209,33 @@ static int acl_check(struct mosquitto_evt_acl_check *ed, MOSQ_FUNC_acl_check che
 	username = mosquitto_client_username(ed->client);
 	clientid = mosquitto_client_clientid(ed->client);
 
+	// First check connectors assigned to clientid or username
 	if(clientid || username){
 		client = dynsec_clients__find(clientid, username);
-		if(client == NULL) return MOSQ_ERR_PLUGIN_DEFER;
-
-		/* Client roles */
-		rc = check(ed, client->rolelist);
-		if(rc != MOSQ_ERR_NOT_FOUND){
-			return rc;
-		}
-
-		HASH_ITER(hh, client->grouplist, grouplist, grouplist_tmp){
-			rc = check(ed, grouplist->group->rolelist);
+		if(client != NULL){
+			rc = check(ed, client->rolelist);
 			if(rc != MOSQ_ERR_NOT_FOUND){
 				return rc;
 			}
+			HASH_ITER(hh, client->grouplist, grouplist, grouplist_tmp){
+				rc = check(ed, grouplist->group->rolelist);
+				if(rc != MOSQ_ERR_NOT_FOUND){
+					return rc;
+				}
+			}
 		}
-	}else if(dynsec_anonymous_group){
+	}
+
+	// No assigned connectors. Now, we check if public access is available
+	// Check if a special anonymous group is configured
+	if(dynsec_anonymous_group){
 		/* If we have a group for anonymous users, use that for checking. */
 		rc = check(ed, dynsec_anonymous_group->rolelist);
 		if(rc != MOSQ_ERR_NOT_FOUND){
 			return rc;
 		}
 	}
-
+	// Fallback to a defaultACLAccess check
 	if(acl_default_access == false){
 		return MOSQ_ERR_PLUGIN_DEFER;
 	}else{
